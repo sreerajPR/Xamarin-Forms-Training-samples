bd66e3ced773a93149e1f0a56905ec8a13864e59
Sreeraj Puthankalathil Ravindranathan <r.sreeraj@noxum.com>
2018-06-19 17:44:54 +0530

    Added XAML Demo

---
+++ XAMLTraining/XAMLTraining/XAMLTraining/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XAMLTraining
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      slider.Minimum = 0;
+      slider.Maximum = 100;
+    }
+    void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+    {
+      label.Text = "Slider value is " + args.NewValue;
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+      slider.Value = 0;
+    }
+  }
+}
