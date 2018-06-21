bb67da27d4c2b4b50f9c736ec79d7b6a6c4fa92c
Sreeraj Puthankalathil Ravindranathan <r.sreeraj@noxum.com>
2018-06-21 19:22:51 +0530

    Added DataBinding Demo

---
+++ DataBindingDemo/DataBindingDemo/DataBindingDemo/MVVMDemo.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataBindingDemo
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class MVVMDemo : ContentPage
+  {
+    public MVVMDemo()
+    {
+      InitializeComponent();
+
+      BindingContext = new ViewModel();
+    }
+  }
+}
---
+++ DataBindingDemo/DataBindingDemo/DataBindingDemo/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DataBindingDemo
+{
+  public class ViewModel : INotifyPropertyChanged
+  {
+    private string firstName;
+    public string FirstName
+    {
+      get { return firstName; }
+      set
+      {
+        if (firstName != value)
+        {
+          firstName = value;
+        // OnPropertyChanged("FullName");
+        }
+      }
+    }
+    private string secondName;
+    public string SecondName
+    {
+      get { return secondName; }
+      set
+      {
+        if (secondName != value)
+        {
+          secondName = value;
+          //OnPropertyChanged("FullName");
+        }
+      }
+    }
+    public string FullName
+    {
+      get { return FirstName + " " + SecondName; }
+    }
+    
+    public ICommand AppendNames
+    {
+      get
+      {
+        return new Command(() => { OnPropertyChanged("FullName"); });
+
+      }
+    }
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
