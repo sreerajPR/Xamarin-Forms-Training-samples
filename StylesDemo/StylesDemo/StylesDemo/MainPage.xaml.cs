686ad887aa8ebf0e7c9051f2d33dc89986430ef0
Sreeraj Puthankalathil Ravindranathan <r.sreeraj@noxum.com>
2018-06-22 15:16:48 +0530

    Added Styles Demo

---
+++ StylesDemo/StylesDemo/StylesDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StylesDemo
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+
+      //Resources["dynamicLabelStyleResource"] = Resources["tealLabelStyle"];
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+      //Resources["dynamicLabelStyleResource"] = Resources["dynamicLabelStyleResource"] == Resources["tealLabelStyle"] ? Resources["redLabelStyle"] : Resources["tealLabelStyle"];
+    }
+  }
+}
